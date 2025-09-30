331a6a4f0e1ea7ea030dea224a66461dada9e932
MatheusHartschuh <matharwords@gmail.com>
2025-09-30 20:03:14 -0300

    [feature] Criação do projeto usando vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>Calculadora</h1>
+    </div>
+  );
+}
+
+export default App
