8586118090ffd5eb088b8b6f60f0fd4192256aa6
MatheusHartschuh <matharwords@gmail.com>
2025-10-05 17:00:35 -0300

    [bugfix] Corrigindo Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Display from "./components/Display";
 import Keypad from "./components/Keypad";
 import TrigModal from "./components/TrigModal";
